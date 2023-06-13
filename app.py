365ae4fb49fc74499ae8cf3ddec5b8670a210fab
DrKamthorn <132547814+DrKamthorn@users.noreply.github.com>
2023-06-13 08:49:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -46,5 +46,5 @@ def main():
             file_url = "audio_chunks.zip"
             st.markdown(f"Click [here]({file_url}) to download the audio chunks.")
 
-if __name__ == '__main__':
+if name == '__main__':
     main()
